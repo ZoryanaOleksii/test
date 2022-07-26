b645a3d2f203327c61fda23be4ebe2974d1a9772
ZoryanaOleksii <109979820+ZoryanaOleksii@users.noreply.github.com>
2022-07-26 18:14:37 +0300

    Add files via upload

---
+++ My_name.py
@@ -0,0 +1,12 @@
+txt =    "    /\\        |         |----         \\   /"
+txt1=   "   /  \\       |         |              \\ /"
+txt2=   "  / ---\\      |         |----               "
+txt3=  " /      \\     |         |             /   \\"
+txt4= "/        \\    |____     |----        /     \\"
+
+
+print(txt)
+print(txt1)
+print(txt2)
+print(txt3)
+print(txt4)
