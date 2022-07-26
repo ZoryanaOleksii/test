7e8b1511b44e94f342c5e83927e74d3f412788e3
ZoryanaOleksii <109979820+ZoryanaOleksii@users.noreply.github.com>
2022-07-26 19:49:25 +0300

    Add files via upload

--- input.py
+++ input.py
@@ -1,9 +1,9 @@
-print("Hi, my name is Oleksii. And what is your name?")
-integer = input('What is your name ' )
+print("Hi, my name is Oleksii.")
+integer = input('And what is your name ' )
 
 print ('Hello  ' + integer )
 integer1 = input('How old are you?')
 var = 2022 - int(integer1)
 var1 = str(var)
 
-print('You are born in ' + var1 + 'year')
+print('You are born in ' + var1 + '  year')
