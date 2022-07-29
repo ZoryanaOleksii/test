b88228f2af5b113cb01e99c403a25de8551cc68b
Chumak Oleksii <massager.ukraine@gmail.com>
2022-07-29 09:44:40 +0300

    first version

---
+++ lesson_4/apple.py
@@ -0,0 +1,13 @@
+# put the number of pupils
+K = int(input('How many pupils we have? '))
+
+# put the number of apples
+N = int(input(' How many apples we have? '))
+
+# number of apples to 1 pupil
+var = N // K
+print('Every pupil have ' +str(var) + ' apples')
+
+# number of apples left in box
+var1 = N % K
+print (str(var1) + ' aplles left in box')
