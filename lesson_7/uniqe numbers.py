5c3fd702ba3b53818a479d5ec94b6f0431b1235b
Chumak Oleksii <massager.ukraine@gmail.com>
2022-08-14 10:05:49 +0300

    can import array with 15 random int

--- lesson_7/uniqe numbers.py
+++ lesson_7/uniqe numbers.py
@@ -3,11 +3,16 @@ var2 = list(input('Input the second list'))
 
 var3 = var1 + var2
 
-
-x = 0
+x = 1
 for x in var3:
     if var3.count(x) == 1:
+        print(var3)
 
-        print(x)
+
+
+
+
+
+
 
 
