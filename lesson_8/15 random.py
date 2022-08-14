66b667d2ec94c1cf285d6f5969bedb8a4bbd4351
Chumak Oleksii <massager.ukraine@gmail.com>
2022-08-14 13:10:02 +0300

    odd and even realised

--- lesson_8/15 random.py
+++ lesson_8/15 random.py
@@ -6,3 +6,7 @@ array = []
 for el in range(15):
     array.append(random.randrange(0, 15, 1))
 print(array)
+odd = len([i + 1 for i in array if i%2 == 0])
+print('Number of odd number is ' + str(odd))
+even = len([i + 1 for i in array if i%2 != 0])
+print('Number of even number is ' + str(even))
