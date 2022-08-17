b21e03e5c171a628caa5b363758ba83182ed0c67
Chumak Oleksii <massager.ukraine@gmail.com>
2022-08-17 19:39:54 +0300

    sum ok

--- lesson_8/random matrix.py
+++ lesson_8/random matrix.py
@@ -1,13 +1,17 @@
 import random
 
 
-N = input('Input number')
+N = int(input('Input number'))
 matrix = [[random.randint(0, 10) for _ in range(N)] for _ in range(N)]
 
 print(matrix)
 
 g = sum((i[-1] for i  in matrix ))
-print(g)
+
+print( 'Sum of last column = '+ str(g))
+i = 1
+s = sum(matrix[i][i] for i in range(N))
+print('Sum of diagonal = ' + str(s))
 
 
 
