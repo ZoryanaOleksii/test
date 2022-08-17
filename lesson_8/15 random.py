6edab870b154632455437c65b5155ba7e07d7b6f
Chumak Oleksii <massager.ukraine@gmail.com>
2022-08-17 19:00:57 +0300

    sum

--- lesson_8/15 random.py
+++ lesson_8/15 random.py
@@ -4,13 +4,13 @@ import random
 el = 1
 array = []
 for el in range(15):
-    array.append(random.randrange(0, 15, 1))
+    array.append(random.randrange(1, 100, 1))
 print(array)
-odd = len([i + 1 for i in array if i%2 == 0])
-print('Number of odd number is ' + str(odd))
-even = len([i + 1 for i in array if i%2 != 0])
-print('Number of even number is ' + str(even))
-
+odd = sum([i for i in array if i%2 == 0])
+print('The sum of odd number is ' + str(odd))
+even = sum([i for i in array if i%2 != 0])
+print('The sum of  even number is ' + str(even))
+#
 if even > odd:
     print('True')
 else:
