7b3e37aa53d1b1135ffd693a4ee27fc8ef86f130
Chumak Oleksii <massager.ukraine@gmail.com>
2022-08-28 20:51:56 +0300

    foo1

--- lesson_12/1.py
+++ lesson_12/1.py
@@ -4,23 +4,28 @@ m = 4
 matrix = [[random.randrange(1, 50) for _ in range(m)] for _ in range(m)]
 # print(matrix)
 
-print(matrix)
-print(list(zip(*matrix)))
-print(list(map(sum, zip(*matrix))))
+# print(matrix)
+# print(list(zip(*matrix)))
+# print(list(map(sum, zip(*matrix))))
 
 
 
-matrix = matrix + [list(map(sum, zip(*matrix)))]
+# matrix = matrix + [list(map(sum, zip(*matrix)))]
 
-
-for i in range(m - 1):
-    for j in range(m - i - 1):
-        if matrix[j] > matrix[j + 1]:
-                matrix[j], matrix[j + 1] = matrix[j + 1], matrix[j]
-
-print(matrix)
+for i in range(len(matrix)):
+    for j in matrix:
+        if matrix[i][j] //2  == 0:
+            print(matrix)
 
 
 
-print('\n'.join('\t'.join(map(str, row)) for row in matrix))
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[i])):
+#         if matrix[i][j] > matrix[i][j + 1]:
+#             matrix[j], matrix[j + 1] = matrix[j + 1], matrix[j]
+#         print(matrix)
+
+
+
+# print('\n'.join('\t'.join(map(str, row)) for row in matrix))
 
